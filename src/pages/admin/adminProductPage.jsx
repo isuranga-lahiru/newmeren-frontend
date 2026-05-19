9fd77fe43732d5245266f83e427beedf70cc4e23
isuranga-lahiru <isurangalahiru1@gmail.com>
2026-05-20 00:51:51 +0530

    Add AdminProductPage component with a floating action button for adding products✏️

---
+++ src/pages/admin/adminProductPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa6'
+
+export default function AdminProductPage() {
+  return (
+    <div className='w-full h-full'>AdminProductPage
+    <button className="bg-accent w-[50px] h-[50px] rounded-full text-white justify-center items-center flex text-2xl fixed bottom-10 right-10 shadow-2xl hover:bg-secondary transition-colors duration-300">
+        <FaPlus />
+      </button>
+    
+    
+    </div>
+  )
+}
+
